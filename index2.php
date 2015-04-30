76574358ebb08fe3bd284da40a35ccf0ecd4d5aa
nalingehlot <nalin.gehlot@gmail.com>
2015-04-30 15:10:38 +0530

    added index2.php

---
+++ index2.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <ul>
+            <li> Saurabh </li>
+            <li> Vijit </li>
+            <li> Ashish </li>
+            <li> Amit </li>
+        </ul>
+    </body>
+</html>
