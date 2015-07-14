884ca3854b56918c33edfaa0ef17125437134b53
nalingehlot <nalin.gehlot@gmail.com>
2015-07-14 11:37:16 +0530

    modified file index

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
             <li> Pradeep Nama Wrong</li>
             <li> Pradeep Nama Wrong</li>
             <li> Prem12344 </li>
-            <li> Mahavir </li>
+            <li> Mahavir123 </li>
         </ul>
     </body>
 </html>
